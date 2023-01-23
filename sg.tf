b723381f410c699a419e9fd8a4019fadbebd9a8b
Anthony Corriero <anthony.corriero@gmail.com>
2023-01-23 16:24:30 -0500

    additional fixes for sg.tf

--- sg.tf
+++ sg.tf
@@ -1,80 +1,80 @@
 resource "aws_security_group" "elb-sg-bean" {
   name = "elb-sg-bean"
   vpc_id = module.vpc.vpc_id
-  egress = [ {
+  egress {
     cidr_blocks = [ "0.0.0.0/0" ]
     description = "outbound"
     from_port = 0
     protocol = "-1"
     to_port = 0
-  } ]
+  }
 
-  ingress = [ {
+  ingress {
     cidr_blocks = [ "0.0.0.0/0" ]
     description = "inbound rules"
     from_port = 80
     protocol = "tcp"
     to_port = 80
-  } ]
+  }
 }
 
 resource "aws_security_group" "bastion-sg" {
   name = "bastion-sg"
   vpc_id = module.vpc.vpc_id
-  egress{
+  egress {
     cidr_blocks = [ "0.0.0.0/0" ]
     from_port = 0
     protocol = "-1"
     to_port = 0
   }
-  ingress = [ {
-    cidr_blocks = [ var.bastion_ip ]
-    description = "outbound"
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
-  } ]
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.bastion_ip]
+  }
 }
 
 resource "aws_security_group" "bs-instance-sg" {
   name = "bean-instance-sg"
   vpc_id = module.vpc.id
-  egress = [ {
+  egress {
     cidr_blocks = [ "0.0.0.0/0" ]
     from_port = 0
     protocol = "-1"
     to_port = 0
-  } ]
-  ingress = [ {
+  }
+  ingress {
     from_port = 22
     protocol = "tcp"
     to_port = 22
     aws_security_group = [aws_security_group.bastion-sg.id]
-  } ]
+  }
 }
 
 resource "aws_security_group" "backend-sg" {
   name = "bean-instance-sg"
   vpc_id = module.vpc.id
-  egress = [ {
+  egress {
     cidr_blocks = [ "0.0.0.0/0" ]
     from_port = 0
     protocol = "-1"
     to_port = 0
-  } ]
-  ingress = [ {
+  }
+  ingress {
     from_port = 0
     protocol = "-1"
     to_port = 0
     aws_security_group = [aws_security_group.bs-instance-sg.id]
-  } ]
+  }
 }
 
 resource "aws_security_group_rule" "sec_group_allow_itself" {
-  type = ingress
-  from_port = 0
-  to_port = 0
-  protocol = "tcp"
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
   security_group_id = aws_security_group.backend-sg.id
   source_security_group_id = aws_security_group.backend-sg.id
 }
+
